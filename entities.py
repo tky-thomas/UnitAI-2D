6ba0d6eea11e7d87f9b6f122182924045eadc25f
ThomasKY <kythomas35@gmail.com>
2023-05-04 14:21:46 +0800

    feature: Added obstacles and generation

--- entities.py
+++ entities.py
@@ -36,8 +36,27 @@ class Player(arcade.Sprite):
 
         self.grid_width = grid_width
 
-    def draw(self, **kwargs):
+    def draw(self, filter=None, pixelated=None, blend_function=None):
         # Draws a red square
         arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height,
                                      arcade.color.RED)
+
+
+class Obstacle(arcade.Sprite):
+    def __init__(self, spawn_pos_grid, grids_width, grids_height, grid_width=20):
+        super().__init__()
+        self.pos = spawn_pos_grid
+
+        # Position the player
+        self.width = grids_width * grid_width
+        self.height = grids_height * grid_width
+        self.center_x = (spawn_pos_grid[0] * grid_width) + (self.width / 2)
+        self.center_y = (spawn_pos_grid[1] * grid_width) + (self.height / 2)
+
+        self.grid_width = grid_width
+
+    def draw(self, **kwargs):
+        # Draws a yellow square
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height,
+                                     arcade.color.YELLOW)
 
