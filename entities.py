8059b46a8d4fccc9bd00ff39faa48115dbb00e2b
ThomasKY <kythomas35@gmail.com>
2023-05-05 15:27:40 +0800

    feature: Added entity mapping

--- entities.py
+++ entities.py
@@ -1,27 +1,6 @@
 import arcade
 
 
-class Grid:
-    def __init__(self, window_width, window_height, grid_width):
-        self.window_width = window_width
-        self.window_height = window_height
-        self.grid_width = grid_width
-        self.toggle = False
-
-    def draw(self):
-        if self.toggle is False:
-            return
-
-        # Draws a gray-bordered grid to help visualize the map
-        for i in range(0, self.window_height, self.grid_width):
-            arcade.draw_line(0, i, self.window_width, i, color=arcade.color.GRAY, line_width=self.grid_width / 10)
-        for i in range(0, self.window_width, self.grid_width):
-            arcade.draw_line(i, 0, i, self.window_height, color=arcade.color.GRAY, line_width=self.grid_width / 10)
-
-    def toggle_grid(self):
-        self.toggle = not self.toggle
-
-
 class Player(arcade.Sprite):
 
     def __init__(self, spawn_pos_grid, grid_width=20):
@@ -81,4 +60,7 @@ class Enemy(arcade.Sprite):
         # Represented as a blue square
         arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height,
                                      arcade.color.BLUE)
+
+    def on_update(self, delta_time: float = 1 / 60):
+        pass
 
--- environment.py
+++ environment.py
@@ -4,6 +4,8 @@ Responsible for maintaining a list of all entities,
 and relaying this information to game objects.
 Also, responsible for generating new enemy entities as they are killed.
 """
+import math
+
 import arcade.sprite
 import numpy as np
 import random
@@ -17,6 +19,10 @@ MAP_ID = 0
 ENEMY_COUNT = 10
 SPAWN_RADIUS = 4
 
+PLAYER = 1
+ENEMY = 2
+OBSTACLE = 3
+
 
 class Environment:
     def __init__(self, window_width, window_height):
@@ -28,6 +34,8 @@ class Environment:
         self.grids_y = round(window_height / GRID_SIZE)
         self.grid = np.zeros((self.grids_y, self.grids_x))
         self.enemy_spawn_grid = None
+
+        self.show_grid = False
 
         self.player = None
         self.obstacles = None
@@ -44,6 +52,8 @@ class Environment:
         for obstacle in MAP_OBSTACLES[MAP_ID]:
             obstacle_sprite = Obstacle((obstacle[0], obstacle[1]), obstacle[2], obstacle[3])
             self.obstacles.append(obstacle_sprite)
+
+        # Gets an obstacle list and exclude enemy spawnpoints from these locations
 
         # Sets up a list to spawn enemies
         # Enemies will be spawned along the borders of the map
@@ -68,11 +78,68 @@ class Environment:
         for enemy in self.enemies:
             enemy.draw()
 
+        if self.show_grid:
+            self.draw_grid()
+
+    def update(self, delta_time):
+        # Updates the player and enemies
+        self.player.update()
+        self.enemies.on_update(delta_time)
+
     def get_map(self):
-        pass
+        # Start with an empty grid
+        grid = np.zeros((self.grids_y, self.grids_x))
+
+        # Loops through all the grid positions, checking for obstacle collisions
+        for obstacle in self.obstacles:
+            grid_positions = get_grid_positions_box(obstacle)
+            for pos in grid_positions:
+                grid[pos[1]][pos[0]] = OBSTACLE
+
+        # Loops through all the player and enemy entities
+        x, y = get_grid_pos(self.player)
+        grid[y][x] = PLAYER
+
+        for enemy in self.enemies:
+            x, y = get_grid_pos(enemy)
+            grid[y][x] = ENEMY
+
+        return grid
 
     def get_entity_list(self):
         pass
 
     def get_gridsize(self):
         return round(self.window_width / len(self.grid[0]))
+
+    def draw_grid(self):
+        # Draws a gray-bordered grid to help visualize the map
+        for i in range(0, self.window_height, GRID_SIZE):
+            arcade.draw_line(0, i, self.window_width, i, color=arcade.color.GRAY, line_width=GRID_SIZE / 10)
+        for i in range(0, self.window_width, GRID_SIZE):
+            arcade.draw_line(i, 0, i, self.window_height, color=arcade.color.GRAY, line_width=GRID_SIZE / 10)
+
+        # Draws a number of top of every entity on the grid
+
+
+    def toggle_grid(self):
+        self.show_grid = not self.show_grid
+
+
+def get_grid_pos(sprite: arcade.Sprite):
+    return math.floor(sprite.center_x / GRID_SIZE), math.floor(sprite.center_y / GRID_SIZE)
+
+
+def get_grid_positions_box(sprite):
+    sprite_bottom_left_pos = (round((sprite.center_x - (sprite.width / 2)) / GRID_SIZE),
+                              round((sprite.center_y - (sprite.height / 2)) / GRID_SIZE))
+    sprite_top_right_pos = (round((sprite.center_x + (sprite.width / 2)) / GRID_SIZE),
+                            round((sprite.center_y + (sprite.height / 2)) / GRID_SIZE))
+
+    grid_positions = list()
+
+    for row in range(sprite_bottom_left_pos[1], sprite_top_right_pos[1]):
+        for column in range(sprite_bottom_left_pos[0], sprite_top_right_pos[0]):
+            grid_positions.append((column, row))
+
+    return grid_positions
